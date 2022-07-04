5bdb79f6e675ac6728f66b9c0cdaa088fe4aa069
Gilberto Stankiewicz <874134+kiewic@users.noreply.github.com>
2022-07-04 02:01:17 -0500

    Initial (working) version of the extension

---
+++ README.md
@@ -0,0 +1,7 @@
+Bing to Google Chrome Extension
+===============================
+
+TODO
+----
+
+1. Update hyperlink when Bing search term changes
---
+++ content-script.js
@@ -0,0 +1,26 @@
+function getSearchTerm() {
+  let search = document.location.search;
+  if (search) {
+    pairs = search.substring(1).split('&');
+    for (let pair of pairs) {
+      pair = pair.split('=');
+      if (pair[0] === 'q') {
+        return pair[1];
+      }
+    }
+  }
+}
+var div = document.createElement("div");
+document.body.appendChild(div);
+div.style.position = "fixed";
+div.style.bottom = "0px";
+div.style.right = "0px";
+div.style.padding = "10px";
+div.style.zIndex = "100";
+div.style.color = "hsla(146, 100%, 20%, 1.0)";
+div.style.backgroundColor = "#01FF70";
+
+var anchor = document.createElement("a");
+div.appendChild(anchor);
+anchor.innerText = "Switch to Google";
+anchor.href = `https://www.google.com/search?q=${getSearchTerm()}`;
